1750211b85ffa3331ecd030817f5d302cf8cd90b
Patrick Allaire <patrick@pallaire.com>
2025-06-12 13:43:34 -0700

    scan2ocr removed the auto open of the pdf

--- bin/scan2ocr.sh
+++ bin/scan2ocr.sh
@@ -9,13 +9,13 @@ tmpname="${destwork}/scanpages.pdf"
 outname="${destination}/${workdt}_scan.pdf"
 
 if [ ! -d "$destination" ]; then
-	echo "Error: Folder destination path does not exist: $destination"
-	exit 1
+  echo "Error: Folder destination path does not exist: $destination"
+  exit 1
 fi
 
 if [ ! -d "$destdeleted" ]; then
-	echo "Creating working folders"
-	mkdir -p $destdeleted
+  echo "Creating working folders"
+  mkdir -p $destdeleted
 fi
 
 # Scan all the page with duplex to temp PNGs
@@ -23,25 +23,25 @@ scanimage --device-name="fujitsu:ScanSnap iX500:4624" --format=jpeg --mode Color
 
 # delete white pages
 for i in "${destwork}/scanpages"*.jpg; do
-	echo "Testing page ${i} for blanks"
-	histogram=$(magick "${i}" -threshold 50% -format %c histogram:info:-)
+  echo "Testing page ${i} for blanks"
+  histogram=$(magick "${i}" -threshold 50% -format %c histogram:info:-)
 
-	# Get the white and black values from the histogram
-	white=$(echo "${histogram}" | grep "white" | sed -n 's/^ *\(.*\):.*$/\1/p')
-	black=$(echo "${histogram}" | grep "black" | sed -n 's/^ *\(.*\):.*$/\1/p')
+  # Get the white and black values from the histogram
+  white=$(echo "${histogram}" | grep "white" | sed -n 's/^ *\(.*\):.*$/\1/p')
+  black=$(echo "${histogram}" | grep "black" | sed -n 's/^ *\(.*\):.*$/\1/p')
 
-	if [ -z $black ]; then
-		black=0
-	fi
+  if [ -z $black ]; then
+    black=0
+  fi
 
-	# Calculate using bc, the percentage of black, if it is under 1/2 a percent, it is white
-	blank=$(echo "scale=4; ${black}/${white} < 0.005" | bc)
+  # Calculate using bc, the percentage of black, if it is under 1/2 a percent, it is white
+  blank=$(echo "scale=4; ${black}/${white} < 0.005" | bc)
 
-	if [ ${blank} -eq "1" ]; then
-		pagenumber=$(echo "$i" | sed 's/[^0-9]//g')
-		echo "${pagenumber} is blank removing it"
-		mv "${i}" "${destdeleted}/${workdt}_${pagenumber}.jpg"
-	fi
+  if [ ${blank} -eq "1" ]; then
+    pagenumber=$(echo "$i" | sed 's/[^0-9]//g')
+    echo "${pagenumber} is blank removing it"
+    mv "${i}" "${destdeleted}/${workdt}_${pagenumber}.jpg"
+  fi
 done
 
 # Merge all images to pdf
@@ -55,4 +55,4 @@ rm "${destwork}/scanpages"*.jpg
 rm "${destwork}/scanpages.pdf"
 
 # Show the new file
-pdfarranger $outname &
+# pdfarranger $outname &
