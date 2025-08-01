a307c3b80388f5cfbd4d0957c6ad2b971bcd94e7
Patrick Allaire <patrick@pallaire.com>
2025-08-01 11:35:51 -0700

    New script to find the fastest arch mirrors

---
+++ bin/fastestarchmirror.sh
@@ -0,0 +1,3 @@
+reflector --country US,CA --number 10 --age 5 --latest 100 --sort rate --thread 4 --protocol https --verbose --save ~/fastestmirrors
+
+echo "Fastest mirrors are listed in the file ~/fastestmirrors"
